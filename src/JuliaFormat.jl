8983b1ba64620f88756101629b35c2c54e5018a5
Eric Davies <iamed2@gmail.com>
2016-03-06 23:50:53 -0600

    Starting with basic function call formatting.

--- src/JuliaFormat.jl
+++ src/JuliaFormat.jl
@@ -1,5 +1,77 @@
 module JuliaFormat
 
-# package code goes here
+export format
+
+import Base: show_unquoted
+
+type FormatError <: Exception
+    msg::AbstractString
+end
+
+expr_and_type(x::Expr, expr_type::Symbol) = x.head === expr_type
+expr_and_type(x, expr_type::Symbol) = false
+
+function format{T<:AbstractString}(code_string::T)
+    code_array = T[]
+    fragment, parse_start_index = parse(code_string, 1)
+    while fragment !== nothing
+        push!(code_array, format(fragment))
+
+        fragment, parse_start_index = parse(code_string, parse_start_index)
+    end
+
+    return join(code_array, "\n")
+end
+
+format(code::Expr) = format(code, Val{code.head}())
+
+format(var) = string(var)
+
+function format(code::Expr, ::Val{:call})
+    buffer = IOBuffer()
+
+    nargs = length(code.args) - 1
+    print(buffer, code.args[1], '(')
+    if nargs > 0
+        kwargs = Any[]
+        semicolon_kwargs = Any[]
+        # todo: make this a map and join
+        for i = 2:nargs
+            if expr_and_type(code.args[i], :parameters)
+                # kwargs after a semicolon
+                semicolon_kwargs = code.args[i].args
+            elseif expr_and_type(code.args[i], :kw)
+                push!(kwargs, code.args[i])
+            else
+                print(buffer, format(code.args[i]))
+                print(buffer, ", ")
+            end
+        end
+        if expr_and_type(code.args[end], :parameters)
+            semicolon_kwargs = code.args[end].args
+        elseif expr_and_type(code.args[end], :kw)
+            push!(kwargs, code.args[end])
+        else
+            print(buffer, format(code.args[end]))
+        end
+
+        append!(kwargs, semicolon_kwargs)
+        if !isempty(kwargs)
+            print(buffer, "; ")
+            print_joined(buffer, map(format, kwargs), ", ")
+        end
+    end
+    print(buffer, ')')
+
+    return takebuf_string(buffer)
+end
+
+function format(code::Expr, ::Val{:...})
+    return "$(format(code.args[1]))..."
+end
+
+function format(code::Expr, ::Val{:kw})
+    return "$(format(code.args[1]))=$(format(code.args[2]))"
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,44 @@
 using JuliaFormat
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@testset "in == out" begin
+    same_strs = [
+        "sum()",
+        "foo(a, b)",
+        "bar(x...)",
+        "baz(x; y=3)",
+        "foobar(a, b, c...)\nx(x)"
+    ]
+
+    for same_str in same_strs
+        @test same_str == format(same_str)
+    end
+end
+
+@testset "whitespace" begin
+    code_pairs = [
+        ("sum( )", "sum()"),
+        ("foo(a,b)", "foo(a, b)"),
+        ("foo( a, b )", "foo(a, b)"),
+        ("bar(x ...)", "bar(x...)"),
+        ("bar(x... )", "bar(x...)"),
+        ("baz(x;y=3)", "baz(x; y=3)"),
+        ("baz(x ; y = 3)", "baz(x; y=3)"),
+        ("baz( ;x =3)", "baz(; x=3)")
+    ]
+
+    for (code_in, code_out) in code_pairs
+        @test code_out == format(code_in)
+    end
+end
+
+@testset "transformations" begin
+    code_pairs = [
+        ("foo(a=3, b=4)", "foo(; a=3, b=4)"),
+        ("foo(a=3; b=4)", "foo(; a=3, b=4)")
+    ]
+
+    for (code_in, code_out) in code_pairs
+        @test code_out == format(code_in)
+    end
+end
